385f136dedd069e81a2ba58d8fb9e275fb5d0a06
musnows <ezplayingd@126.com>
2025-01-30 14:16:03 +0800

    Fix: fix path error for sed in pytorch install.sh

--- docker/install/pytorch/install_pytorch.sh
+++ docker/install/pytorch/install_pytorch.sh
@@ -8,12 +8,12 @@ pip3 install pyyaml
 
 git clone --recursive -b v1.0.1 https://github.com/pytorch/pytorch
 pushd pytorch
+    # 修改不兼容的pyyaml代码
+    sed -i "s|yaml.load('\n'.join(declaration_lines))|yaml.load('\n'.join(declaration_lines),Loader=yaml.FullLoader)|" ./aten/src/ATen/cwrap_parser.py
+    sed -i "s|yaml.load('\n'.join(declaration_lines))|yaml.load('\n'.join(declaration_lines),Loader=yaml.FullLoader)|" ./tools/cwrap/cwrap.py
+    # 开始编译libtorch
     rm -rf build
     mkdir build && cd build
-    # 修改不兼容的pyyaml代码
-    sed -i "s|yaml.load('\n'.join(declaration_lines))|yaml.load('\n'.join(declaration_lines),Loader=yaml.FullLoader)|" aten/src/ATen/cwrap_parser.py
-    sed -i "s|yaml.load('\n'.join(declaration_lines))|yaml.load('\n'.join(declaration_lines),Loader=yaml.FullLoader)|" tools/cwrap/cwrap.py
-    # 开始编译libtorch
     python3 ../tools/build_libtorch.py
 ldconfig
 popd
