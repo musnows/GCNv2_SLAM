fdfff15532e3612873f60675c34b33ee32d43e69
musnows <ezplayingd@126.com>
2025-01-30 15:05:24 +0800

    Fix: add dockerfile.base and fix sed error in install pytorch

--- docker/install/pytorch/install_pytorch.sh
+++ docker/install/pytorch/install_pytorch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 # 编译运行需要
@@ -9,8 +10,8 @@ pip3 install pyyaml
 git clone --recursive -b v1.0.1 https://github.com/pytorch/pytorch
 pushd pytorch
     # 修改不兼容的pyyaml代码
-    sed -i "s|yaml.load('\n'.join(declaration_lines))|yaml.load('\n'.join(declaration_lines),Loader=yaml.FullLoader)|" ./aten/src/ATen/cwrap_parser.py
-    sed -i "s|yaml.load('\n'.join(declaration_lines))|yaml.load('\n'.join(declaration_lines),Loader=yaml.FullLoader)|" ./tools/cwrap/cwrap.py
+    sed -i "s/yaml.load(\(.*\))/yaml.load(\1, Loader=yaml.FullLoader)/g" aten/src/ATen/cwrap_parser.py
+    sed -i "s/yaml.load(\(.*\))/yaml.load(\1, Loader=yaml.FullLoader)/g" tools/cwrap/cwrap.py
     # 开始编译libtorch
     rm -rf build
     mkdir build && cd build
